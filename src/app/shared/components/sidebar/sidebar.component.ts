fb36083b20c0b4f69229f74c447b8e08f306e4d0
Nestor Bardel <nestorbardel@gmail.com>
2024-08-26 02:45:27 -0300

    first commit

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SidebarComponent { }
