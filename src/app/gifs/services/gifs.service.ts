d490bd138e9c5b76610130aea64ed50c46d59d98
Nestor Bardel <nestorbardel@gmail.com>
2024-10-13 02:16:18 -0300

    98 - terminada

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,13 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+
+  public gifList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
 
@@ -39,9 +42,10 @@ export class GifsService {
     .set('limit', '10')
     .set('q', tag);
 
-    this.http.get(`${this.serviceUrl}/search`, {params})
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
     .subscribe(resp => {
-      console.log(resp);
+      this.gifList = resp.data;
+      console.log({gifs: this.gifList});
 
     })
   }
