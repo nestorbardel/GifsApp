eb5884e1fa4dbdf9c2c969ea56c397981d96ac4a
Nestor Bardel <nestorbardel@gmail.com>
2024-10-19 03:31:35 -0300

    109 - en proceso

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import {Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: `./lazy-image.component.html`,
+})
+export class LazyImageComponent implements OnInit{
+  @Input() public url!: string;
+
+  @Input() public alt: string = '';
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('URL Property is required');
+  }
+ }
